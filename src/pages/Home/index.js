c962af26fdb06f4397204cb8e120da179aa0f2d8
ken_okubo <ken.okubo1@gmail.com>
2026-04-09 13:11:35 -0300

    hasError message

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,8 +3,16 @@ import { Link } from "react-router-dom";
 import arrow from "../../assets/images/icons/arrow.svg";
 import edit from "../../assets/images/icons/edit.svg";
 import trash from "../../assets/images/icons/trash.svg";
+import sad from "../../assets/images/sad.svg";
 import { Container } from "../../components/App/styles";
-import { Card, Header, InputSearchContainer, ListHeader } from "./styles";
+import Button from "../../components/Button";
+import {
+  Card,
+  ErrorContainer,
+  Header,
+  InputSearchContainer,
+  ListHeader,
+} from "./styles";
 
 import Loader from "../../components/Loader";
 import ContactsService from "../../services/ContactsService";
@@ -14,6 +22,7 @@ export default function Home() {
   const [orderBy, setOrderBy] = useState("asc");
   const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   // useMemo é um hook do React que memoriza o resultado de uma função e só a reexecuta quando as dependências especificadas mudam.
   // Ele é útil para otimizar o desempenho, evitando cálculos desnecessários em cada renderização.
@@ -26,20 +35,21 @@ export default function Home() {
     );
   }, [contacts, searchTerm]);
 
-  useEffect(() => {
-    async function loadContacts() {
-      try {
-        setIsLoading(true);
+  async function loadContacts() {
+    try {
+      setIsLoading(true);
 
-        const contactsList = await ContactsService.listContacts(orderBy);
+      const contactsList = await ContactsService.listContacts(orderBy);
 
-        setContacts(contactsList);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
+      setContacts(contactsList);
+    } catch (error) {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
     }
+  }
+
+  useEffect(() => {
     loadContacts();
   }, [orderBy]); // o orderBy após a virgula indica que o useEffect deve ser executado sempre que o valor de orderBy for alterado.
 
@@ -49,6 +59,11 @@ export default function Home() {
 
   function handleChangeSearchTerm(event) {
     setSearchTerm(event.target.value);
+  }
+
+  function handleTryAgain() {
+    loadContacts();
+    setHasError(false);
   }
 
   return (
@@ -63,44 +78,65 @@ export default function Home() {
         />
       </InputSearchContainer>
 
-      <Header>
-        <strong>
-          {filteredContacts.length}
-          {filteredContacts.length === 1 ? " contato" : " contatos"}
-        </strong>
+      <Header hasError={hasError}>
+        {!hasError && (
+          <strong>
+            {filteredContacts.length}
+            {filteredContacts.length === 1 ? " contato" : " contatos"}
+          </strong>
+        )}
         <Link to="/new">Novo Contato</Link>
       </Header>
 
-      <ListHeader $orderBy={orderBy}>
-        {filteredContacts.length > 0 && (
-          <button type="button" onClick={handleToggleOrderBy}>
-            <span>Nome</span>
-            <img src={arrow} alt="Arrow" />
-          </button>
-        )}
-      </ListHeader>
+      {hasError && (
+        <ErrorContainer>
+          <img src={sad} alt="Sad" />
+          <div className="details">
+            <span>
+              <strong>Ocorreu um erro ao carregar os contatos.</strong>
+            </span>
 
-      {filteredContacts.map((contact) => (
-        <Card key={contact.id}>
-          <div className="info">
-            <div className="contact-name">
-              <strong>{contact.name}</strong>
-              {contact.category_name && <small>{contact.category_name}</small>}
-            </div>
-            <span>{contact.email}</span>
-            <span>{contact.phone}</span>
+            <Button onClick={handleTryAgain}>Tentar novamente</Button>
           </div>
+        </ErrorContainer>
+      )}
 
-          <div className="actions">
-            <Link to={`/edit/${contact.id}`}>
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-      ))}
+      {!hasError && (
+        <>
+          {filteredContacts.length > 0 && (
+            <ListHeader $orderBy={orderBy}>
+              <button type="button" onClick={handleToggleOrderBy}>
+                <span>Nome</span>
+                <img src={arrow} alt="Arrow" />
+              </button>
+            </ListHeader>
+          )}
+
+          {filteredContacts.map((contact) => (
+            <Card key={contact.id}>
+              <div className="info">
+                <div className="contact-name">
+                  <strong>{contact.name}</strong>
+                  {contact.category_name && (
+                    <small>{contact.category_name}</small>
+                  )}
+                </div>
+                <span>{contact.email}</span>
+                <span>{contact.phone}</span>
+              </div>
+
+              <div className="actions">
+                <Link to={`/edit/${contact.id}`}>
+                  <img src={edit} alt="Edit" />
+                </Link>
+                <button type="button">
+                  <img src={trash} alt="Delete" />
+                </button>
+              </div>
+            </Card>
+          ))}
+        </>
+      )}
     </Container>
   );
 }
